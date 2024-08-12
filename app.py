22b3ad70c0f8a540e7d1ed85eddeca1479676056
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2024-08-12 21:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ nltk.download("punkt_tab")
 nltk.download("punkt")
 nltk.download("stopwords")
 nltk.download("wordnet")
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('omw-1.4')
 from wordcloud import WordCloud
